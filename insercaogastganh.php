963851a56562f16968faf4443da37a089d8fe48f
João Victor Aguiar <168779818+Joao-AguiarN@users.noreply.github.com>
2026-09-03 21:59:31 -0300

    Upload da página de inserção.

---
+++ insercaogastganh.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.8/css/bootstrap.min.css" integrity="sha512-2bBQCjcnw658Lho4nlXJcc6WkV/UxpE/sAokbXPxQNGqmNdQrWqtw26Ns9kFF/yG792pKR1Sx8/Y1Lf1XN4GKA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.8/js/bootstrap.min.js" integrity="sha512-nKXmKvJyiGQy343jatQlzDprflyB5c+tKCzGP3Uq67v+lmzfnZUi/ZT+fc6ITZfSC5HhaBKUIvr/nTLCV+7F+Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    </head>
+
+    <body>
+    <div class="row bg-warning border border-2 border-dark text-dark p-3">
+
+        <div class="col text-center fw-bold">
+            Webt Finance
+        </div>
+
+    </div>
+    <div class="row text-center mb-5 mt-4">
+
+        <h2>O que temos para hoje?</h2>
+
+    </div>
+    </body>
+</html>
